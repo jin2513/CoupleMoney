2a20780625b00c1b08dba84dfa59f2d7b4c1b382
jhgan <jhgan@ST-dev-jhgan.st-company.net>
2016-07-01 16:46:11 +0900

    1. view 레이아웃 : jstl -> thymeleaf 로 변경 2. web config 변경

--- CoupleMoney/src/main/java/test/mvc/spring/config/servlet/WebMvcConfig.java
+++ CoupleMoney/src/main/java/test/mvc/spring/config/servlet/WebMvcConfig.java
@@ -1,56 +1,75 @@
 package test.mvc.spring.config.servlet;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
+//@Configuration
+//@ComponentScan(basePackages = "test.mvc.spring", includeFilters = @Filter({ Controller.class,
+//		Service.class }), useDefaultFilters = false)
+//public class WebMvcConfig extends WebMvcConfigurationSupport {
 @Configuration
-@ComponentScan(basePackages= "test.mvc.spring", includeFilters = @Filter({Controller.class, Service.class}), useDefaultFilters = false)
-public class WebMvcConfig extends WebMvcConfigurationSupport {
-	
-	private static final String[] RESOURCES = {"css", "js", "webjars"};
+@EnableWebMvc
+@ComponentScan("test.mvc.spring")
+public class WebMvcConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+	private static final String[] RESOURCES = { "resources", "webjars" };
+
+	private ApplicationContext applicationContext;
+
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
 	
 	@Override
-	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-		for(String resource: RESOURCES) {
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		for (String resource : RESOURCES) {
 			String resourceHandler = "/" + resource + "/";
 			String resourceLocations = resourceHandler + "**";
-			registry.addResourceHandler(resourceHandler).addResourceLocations(resourceLocations);
+			System.out.println("resourceHandler : " + resourceHandler + " / resourceLocations : " + resourceLocations);
+			registry.addResourceHandler(resourceHandler).addResourceLocations(resourceLocations).setCachePeriod(31556926);
 		}
 	}
 	
 	@Override
-	protected void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
 	}
-	
+
 	@Bean
-	public InternalResourceViewResolver setJspViewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix(".jsp");
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		resolver.setCharacterEncoding("UTF-8");
 		return resolver;
 	}
-	
-	@Override
-	public RequestMappingHandlerMapping requestMappingHandlerMapping() {
-		RequestMappingHandlerMapping requestMappingHandlerMapping = super.requestMappingHandlerMapping();
-		requestMappingHandlerMapping.setUseSuffixPatternMatch(false);
-		requestMappingHandlerMapping.setUseTrailingSlashMatch(false);
-		return requestMappingHandlerMapping;
-	}
-	
+
 	@Bean
-	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
-		return new PropertySourcesPlaceholderConfigurer();
+	public TemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setEnableSpringELCompiler(true);
+		engine.setTemplateResolver(templateResolver());
+		return engine;
+	}
+
+	private ITemplateResolver templateResolver() {
+		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+		resolver.setApplicationContext(applicationContext);
+		resolver.setPrefix("/WEB-INF/templates/");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode(TemplateMode.HTML);
+		return resolver;
 	}
 }
